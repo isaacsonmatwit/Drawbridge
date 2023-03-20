ccca99d2397bbdc871a57e3f18fc795b6d0354aa
badnewshughes <119268388+badnewshughes@users.noreply.github.com>
2023-03-20 13:42:31 -0400

    Update looks

--- sendMessage.js
+++ sendMessage.js
@@ -1,10 +1,10 @@
 function CopyToLabel() {
     //Reference the TextBox.
-    var txtName = document.getElementById("txtName");
+    var txtName = document.getElementById("txtSend");
 
     //Reference the Label.
     document.getElementById("mytextarea").innerHTML +=
-      "&#13;&#10; You: " + txtName.value;
+      "&#13;&#10; You: " + txtSend.value;
 
     //Copy the TextBox value to Label.
   }
