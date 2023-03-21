8927c6056d12b8f0ce733c72e30f754437d04b3f
badnewshughes <119268388+badnewshughes@users.noreply.github.com>
2023-03-21 13:21:09 -0400

    rename landingpage to Login and Show PW and confirm PW

---
+++ loginAndRegister.js
@@ -0,0 +1,24 @@
+var check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('passwordConfirm').value) {
+      document.getElementById('checker').style.color = 'green';
+      document.getElementById('checker').innerHTML = 'matching';
+    } else {
+      document.getElementById('checker').style.color = 'red';
+      document.getElementById('checker').innerHTML = 'not matching';
+    }
+  }
+
+  function togglePW() {
+    var passwordField = document.getElementById("password");
+    var confirmPasswordField = document.getElementById("passwordConfirm");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        confirmPasswordField.type = "text";
+    }
+    else {
+        passwordField.type = "password";
+        confirmPasswordField.type = "password";
+    }
+}
