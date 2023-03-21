50a35988ef75613e811daf44d4f6c6aac644532c
badnewshughes <119268388+badnewshughes@users.noreply.github.com>
2023-03-21 18:47:54 -0400

    Matching doesnt show if field is empty

--- loginAndRegister.js
+++ loginAndRegister.js
@@ -1,29 +1,31 @@
-var check = function() {
-    if (document.getElementById('password').value ==
-      document.getElementById('passwordConfirm').value) {
-      document.getElementById('checker').style.color = 'green';
-      document.getElementById('checker').innerHTML = 'matching';
-    } else {
-      document.getElementById('checker').style.color = 'red';
-      document.getElementById('checker').innerHTML = 'not matching';
-    }
+var check = function () {
+  if (document.getElementById('password').value == "") {
+  } else if (document.getElementById('password').value ==
+    document.getElementById('passwordConfirm').value) {
+    document.getElementById('checker').style.color = 'green';
+    document.getElementById('checker').innerHTML = 'matching';
+  } else {
+    document.getElementById('checker').style.color = 'red';
+    document.getElementById('checker').innerHTML = 'not matching';
   }
 
-  function togglePW() {
-    var passwordField = document.getElementById("password");
-    var confirmPasswordField = document.getElementById("passwordConfirm");
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        confirmPasswordField.type = "text";
-    }
-    else {
-        passwordField.type = "password";
-        confirmPasswordField.type = "password";
-    }
 }
 
-function savePWandusrname(){
+function togglePW() {
+  var passwordField = document.getElementById("password");
+  var confirmPasswordField = document.getElementById("passwordConfirm");
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    confirmPasswordField.type = "text";
+  }
+  else {
+    passwordField.type = "password";
+    confirmPasswordField.type = "password";
+  }
+}
+
+function savePWandusrname() {
   let passwordValue = document.getElementById('password')
   let usernameValue = document.getElementById()
 }
