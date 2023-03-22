3c98c36d7dea9cb5a4d35d06e9af0d96852ae0b9
badnewshughes <119268388+badnewshughes@users.noreply.github.com>
2023-03-21 20:58:13 -0400

    Password Complexity finally works

--- loginAndRegister.js
+++ loginAndRegister.js
@@ -49,30 +49,41 @@ app.post('/register', async (req, res) => {
 });
 
 function checkPWComplexity() {
-  let passwordField = document.getElementById('password');
+  let passwordField = document.getElementById('password').value;
   let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
-
+  
   if (passwordField.length >= 8) {
     document.getElementById('pwLength').style.color = 'green';
+  }else{
+    document.getElementById('pwLength').style.color = '#AF0C0C';
   }
 
-  if (passwordField.match(/a-z/)) {
+  if (passwordField.match(/[a-z]/)) {
     document.getElementById('pwLowerCase').style.color = 'green';
+  }else{
+    document.getElementById('pwLowerCase').style.color = '#AF0C0C';
   }
 
-  if (passwordField.match(/A-Z/)) {
+  if (passwordField.match(/[A-Z]/)) {
     document.getElementById('pwUpperCase').style.color = 'green';
+  }else{
+    document.getElementById('pwUpperCase').style.color = '#AF0C0C';
   }
 
   if (passwordField.match(/\d/)) {
     document.getElementById('pwNumbers').style.color = 'green';
+  }else{
+    document.getElementById('pwNumbers').style.color = '#AF0C0C';
   }
 
   if (passwordField.match(/[^a-zA-Z\d]/)) {
     document.getElementById('pwSpecialChar').style.color = 'green';
+  }else{
+    document.getElementById('pwSpecialChar').style.color = '#AF0C0C';
   }
 
-  if (passwordField.match(strongPassword)) {
-    window.location.replace("index.html");
-  }
+  // if (passwordField.match(strongPassword)) {
+  //   window.location.replace("index.html");
+  // }
+  
 }
