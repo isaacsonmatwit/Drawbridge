772241ef8770dab65edf27845b4e74912faef393
Noah <noah.vernon@gmail.com>
2023-03-24 15:54:45 -0400

    New error aquired

--- public/JavaScript/loginAndRegister.js
+++ public/JavaScript/loginAndRegister.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const sqlite = require('sqlite')
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
