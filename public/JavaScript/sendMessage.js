01e334a17dc1a98c7af0ff7c1f2cb5c360a28596
Vlad The Evil Cat <vjcaso@gmail.com>
2023-03-24 23:43:35 -0400

    textbox is cleared after a message has been sent

--- public/JavaScript/sendMessage.js
+++ public/JavaScript/sendMessage.js
@@ -6,8 +6,8 @@ function CopyToLabel() {
     if (txtSend.value != 0){
       document.getElementById("mytextarea").innerHTML +=
       "&#13;&#10; You: " + txtSend.value;
+      txtSend.value = ''; //Clears textbox so you dont have to!
     }
     
-
     //Copy the TextBox value to Label.
   }
