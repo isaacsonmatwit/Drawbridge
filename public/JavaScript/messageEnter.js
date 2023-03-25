09f197fbe9cc66228331b8200299bc6a3b5a86ad
Vlad The Evil Cat <vjcaso@gmail.com>
2023-03-25 00:47:14 -0400

    It's finally fixed!
    -Pressing enter while the message textbox actually sends the message.

--- public/JavaScript/messageEnter.js
+++ public/JavaScript/messageEnter.js
@@ -1,10 +1,6 @@
 //Pressing enter while the message textbox is active sends your message!
-const txt = document.getElementById("txtSend");
-const btn = document.getElementById("btnSend");
 
-txt.onkeydown = function(e){
-    if(e.key=='Enter'){
-        btn.click();
-        txt.value='';
-    }
+document.getElementById('txtSend').onkeydown = function(e){
+    if(e.key=='Enter')
+        document.getElementById('btnSend').click();
 }
