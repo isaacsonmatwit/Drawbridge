52dd8c3a39dd29001eafe68ad4d47922a32de48d
Vlad The Evil Cat <vjcaso@gmail.com>
2023-04-03 17:01:45 -0400

    Update deliciousCookie.js
    -setCookie no longer checks if cookie already exists;
    -addCookie checks if cookie already exists;
    -Functions now return true/false based on the operation's success;
    -Modified some documentation;

--- public/JavaScript/deliciousCookie.js
+++ public/JavaScript/deliciousCookie.js
@@ -1,4 +1,5 @@
-
+// Cookie API
+// @Author Vlad. J. Caso
 const PastDate = 'Thu, 01 Jan 1970 00:00:00 UTC';
 
 //var isLoggedIn = false;
@@ -9,11 +10,11 @@ const getCookiesAsList = () => {
 
 const getThisDate = () => {
   return new Date().toUTCString();
-  
 }
 
 // @Returns the cookie with name (cName); 
 // @Returns null if (cName) not found;
+// Note: returns the cookie with the name: 'cName=cValue;'
 function getCookie(cName){
   const cookieList = getCookiesAsList();
   for (ci in cookieList)
@@ -33,20 +34,23 @@ function getCookieValue(cName){
 function setCookie(cName, cValue, expireDate) {
   if(expireDate==null)
     expireDate = weeksToDate(1);
-  if(findCookie(cName))
-    document.cookie = cName + "=" + cValue + ";" + expireDate;
+  document.cookie = cName + "=" + cValue + ";" + expireDate;
+  return true;
 }
 
 // Sets the value of cookie (cName) to (cValue)
+// Note: Use this instead of {setCookie} to unsure that only the value is set
 function setCookieValue(cName, cValue) {
   if(cookieExists(cName))
-    setCookie(cName,cValue,null);
+    return setCookie(cName,cValue,null);
+  return false;
 }
 
 // Sets the expiration date of the cookie (cName) to (expireDate)
 function setCookieExpiration(cName, expireDate) {
   if(cookieExists(cName))
-    setCookie(cName, getCookieValue(), expireDate);
+    return setCookie(cName, getCookieValue(), expireDate);
+  return false;
 }
 
 // @Returns true if value of cookie (cName) is equal to (forValue), false if not;
@@ -62,16 +66,21 @@ function cookieExists(cName) {
   return getCookie(cName)!=null;
 }
 
-// Adds a new cookie with name (cName) and with value (cValue), which expires on (expireDate);
+// Adds a new cookie with name (cName) and value (cValue), and expiration date (expireDate);
 // Note: function {daysToDate} can be used to convert any number of days to a date; daysToDate = current date + # of days;
 function addCookie(cName, cValue, expireDate) {
-  document.cookie = cName + '=' + cValue + 'expires=' + expireDate;
+  if(!cookieExists(cName)){
+    document.cookie = cName + '=' + cValue + 'expires=' + expireDate;
+    return true;
+  }
+  return false;
 }
 
 // Removes a cookie by setting it's expiration date to the earliest computer date (PastDate)
 function removeCookie(cName) {
   if(cookieExists(cName))
-    setCookie(cName, '', PastDate);
+    return setCookie(cName, '', PastDate);
+  return false;
 }
 
 /*------------------------*\
