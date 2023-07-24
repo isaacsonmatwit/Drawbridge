17c38b351400d0a3b4e7e05330cbdd9cc198f4a7
badnewshughes <119268388+badnewshughes@users.noreply.github.com>
2023-07-24 11:17:34 -0400

    Auth0 added back

--- DrawBridgeV2/public/index.js
+++ DrawBridgeV2/public/index.js
@@ -23,3 +23,29 @@ io.on('connection', (socket) => {
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '5JWggSs98cyBigw+ZtSsA7zQ8OhTup79YIaYH+LAOKR1TW6SFkgAyGARbGhgTyPO',
+  baseURL: 'http://localhost:3000',
+  clientID: 'dlgjMBh6ROXYiTyZy8tW6VkOpwAphB4M',
+  issuerBaseURL: 'https://dev-6c8dnubymkkxrng8.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+  console.log(JSON.stringify(req.oidc.user))
+});
